1274a592470f6ff7067f9a653fff4a388348ce2d
Reggie-L6 <liujingwen2024@bupt.edu.cn>
2026-02-27 17:37:56 +0800

    Update nn.py

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -10,7 +10,7 @@ class Module:
     def parameters(self):
         return []
 
-class Neuron(Module):
+class Neuron(Module):#创造神经元，先是随机选weight和bias
 
     def __init__(self, nin, nonlin=True):
         self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
@@ -18,7 +18,7 @@ class Neuron(Module):
         self.nonlin = nonlin
 
     def __call__(self, x):
-        act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+        act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)#zip可以打包对齐
         return act.relu() if self.nonlin else act
 
     def parameters(self):
@@ -32,19 +32,19 @@ class Layer(Module):
     def __init__(self, nin, nout, **kwargs):
         self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
 
-    def __call__(self, x):
+    def __call__(self, x):#如果这层只有一个神经元，它就直接返回一个值；如果有多个，就返回一个包含所有输出的列表。
         out = [n(x) for n in self.neurons]
         return out[0] if len(out) == 1 else out
 
     def parameters(self):
-        return [p for n in self.neurons for p in n.parameters()]
+        return [p for n in self.neurons for p in n.parameters()]#遍历层里的每一个神经元，把它们各自的 w (权重) 和 b (偏置) 全部汇总到一个大列表里
 
     def __repr__(self):
         return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
 
 class MLP(Module):
 
-    def __init__(self, nin, nouts):
+    def __init__(self, nin, nouts):#输入特征是 3 个，你想建两个隐藏层（各 4 个神经元），最后输出 1 个结果。你传入 nin=3, nouts=[4, 4, 1]
         sz = [nin] + nouts
         self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
 
